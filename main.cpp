508629eaf98e496fba961cea9c97eb834c567d58
Andreas Kantilierakis <163316636+AndreasKantilierakis@users.noreply.github.com>
2025-08-29 10:00:57 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,91 @@
+#include <cassert>
+#include <memory>
+#include <pthread.h>
+#include <cstdlib>
+#include <malloc.h>
+#include <iostream>  
+#include "ssmem.h"
+#include <libvmmalloc.h>
+
+#define NUM_THREADS 8
+
+// per-thread allocators must be declared before including the queue header
+__thread ssmem_allocator_t *alloc;
+__thread ssmem_allocator_t *volatileAlloc;
+
+#include "queues/OptUnlinkedQ.h"
+
+struct threadArgs {
+    OptUnlinkedQ<int> *queue;
+    size_t tid;
+};
+
+void* threadFunc(void* arg) {
+    auto* t_args = static_cast<threadArgs*>(arg);
+    int tid = t_args->tid;
+    auto* queue = t_args->queue;
+    int* dequeued = static_cast<int*>(malloc(sizeof(int)));
+
+    // initialize per-thread allocators
+    alloc = static_cast<ssmem_allocator_t*>(malloc(sizeof(ssmem_allocator_t)));
+    if (alloc == nullptr) {
+        std::cerr << "malloc failed\n";
+        return nullptr;
+    }
+    ssmem_alloc_init(alloc, SSMEM_DEFAULT_MEM_SIZE, tid);
+
+    volatileAlloc = static_cast<ssmem_allocator_t*>(malloc(sizeof(ssmem_allocator_t)));
+    if (volatileAlloc == nullptr) {
+        std::cerr << "malloc2 failed\n";
+        return nullptr;
+    }
+    ssmem_alloc_init(volatileAlloc, SSMEM_DEFAULT_MEM_SIZE, tid);
+
+#ifdef DEBUG
+    std::cout << "thread " << tid << " has started... q = " << queue << "\n";
+#endif
+    assert(alloc && volatileAlloc);
+
+    for (int i = 1; i < 10000; i++) {
+        queue->enq(tid * i, tid);
+        queue->deq(dequeued, tid);
+    }
+
+    return nullptr;
+}
+
+int main() {
+    // pre-initialize allocators for each thread
+    for (int i = 0; i < NUM_THREADS; i++) {
+        alloc = static_cast<ssmem_allocator_t*>(malloc(sizeof(ssmem_allocator_t)));
+        assert(alloc);
+        volatileAlloc = static_cast<ssmem_allocator_t*>(malloc(sizeof(ssmem_allocator_t)));
+        assert(volatileAlloc);
+
+        ssmem_alloc_init(alloc, SSMEM_DEFAULT_MEM_SIZE, i);
+        ssmem_alloc_init(volatileAlloc, SSMEM_DEFAULT_MEM_SIZE, i);
+    }
+
+    // create queue
+    auto* queue = new OptUnlinkedQ<int>();
+
+    pthread_t threads[NUM_THREADS];
+    threadArgs tids[NUM_THREADS];
+
+    for (int i = 0; i < NUM_THREADS; i++) {
+        tids[i].tid = i;
+        tids[i].queue = queue;
+        pthread_create(&threads[i], nullptr, threadFunc, &tids[i]);
+    }
+
+    for (int i = 0; i < NUM_THREADS; i++) {
+        pthread_join(threads[i], nullptr);
+    }
+
+    int* dummy = static_cast<int*>(malloc(sizeof(int)));
+    if (queue->deq(dummy, 0) == false) {
+        std::cout << "empty queue\n";
+    }
+
+    return 0;
+}
